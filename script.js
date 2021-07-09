9f1654d0ded7d8c1c1d4353fa854220d439198e2
Rrjgo <howardrrjgo@gmail.com>
2021-07-09 13:46:44 +1200

    changed to latest message comes first

--- script.js
+++ script.js
@@ -8,35 +8,39 @@ fetch('http://localhost:8000/msg').then((res) => {
     res.json().then((data) => {
 
         data.map(message => {
-            const p = document.createElement('p')
-            p.innerHTML = message.message + " " + message.timestamp
+            const firstChild = messageDiv.firstChild
 
-            messageDiv.appendChild(p)
+            const p = document.createElement('p')
+            p.innerHTML = message.timestamp + " " + message.message
+
+            messageDiv.insertBefore(p, firstChild)
         })
     })
 })
 
 sendBtn.onclick = async () => {
-    const data = {message: document.getElementById("msg").value}
+    const data = { message: document.getElementById("msg").value }
 
     await fetch('http://localhost:8000/msg', {
         body: JSON.stringify(data), // must match 'Content-Type' header
         headers: {
-          'content-type': 'application/json'
+            'content-type': 'application/json'
         },
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
     })
 
     messageDiv.innerHTML = ""
-    
+
     fetch('http://localhost:8000/msg').then((res) => {
         res.json().then((data) => {
 
             data.map(message => {
-                const p = document.createElement('p')
-                p.innerHTML = message.message + " " + message.timestamp
+                const firstChild = messageDiv.firstChild
 
-                messageDiv.appendChild(p)
+                const p = document.createElement('p')
+                p.innerHTML = message.timestamp + " " + message.message
+
+                messageDiv.insertBefore(p, firstChild)
             })
         })
     })
