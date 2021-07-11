8b98337a794334756627b098599cef2022231d02
Rrjgo <howardrrjgo@gmail.com>
2021-07-11 18:09:37 +1200

    attempt on srarch

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 console.log("Billboard")
 
 const messageDiv = document.getElementById("message")
-const sendBtn = document.getElementById('btn')
+const sendBtn = document.getElementById('sendBtn')
+
+const searchDiv = document.getElementById("search")
+const searchBtn = document.getElementById('searchBtn')
 
 fetch('http://localhost:8000/msg').then((res) => {
     res.json().then((data) => {
@@ -18,7 +21,7 @@ fetch('http://localhost:8000/msg').then((res) => {
 })
 
 sendBtn.onclick = async () => {
-    const data = { message: document.getElementById("msg").value }
+    const data = { message: document.getElementById("textBox").value }
 
     await fetch('http://localhost:8000/msg', {
         body: JSON.stringify(data), // must match 'Content-Type' header
@@ -43,4 +46,34 @@ sendBtn.onclick = async () => {
             })
         })
     })
+}
+
+searchBtn.onclick = async () => {
+    const searchdata = { message: document.getElementById("searchBox").value }
+
+    await fetch('http://localhost:8000/msg', {
+        body: JSON.stringify(searchdata), // must match 'Content-Type' header
+        headers: {
+            'content-type': 'application/json'
+        },
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    })
+
+    searchDiv.innerHTML = ""
+
+    fetch('http://localhost:8000/msg').then((res) => {
+        res.json().then((data) => {
+
+            data.map(message => {
+                const firstChild = searchDiv.firstChild
+
+                const p = document.createElement('p')
+                p.innerHTML = message.timestamp + " " + message.message
+
+                searchDiv.insertBefore(p, firstChild)
+            })
+        })
+    })
+    
+
 }
