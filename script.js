fcc0fd9edf8a5afa8d637e495049086b75193080
Howard Jiang <howardrrjgo@gmail.com>
2021-07-14 14:29:28 +1200

    finished search feature

--- script.js
+++ script.js
@@ -8,18 +8,28 @@ const sendBtn = document.getElementById('sendBtn')
 const searchDiv = document.getElementById("search")
 const searchBtn = document.getElementById('searchBtn')
 
-fetch(`${endpoint}/msg`).then((res) => {
-    res.json().then((data) => {
+const fetchJSON = async (url, params) => {
+    return (await fetch(url, params)).json()
+}
 
-        data.map(message => {
-            const firstChild = messageDiv.firstChild
+fetchJSON(`${endpoint}/msg`).then((data) => {
+    data.map(message => {
+        const firstChild = messageDiv.firstChild
 
-            const p = document.createElement('p')
-            p.innerHTML = message.timestamp + " " + message.message
+        const p = document.createElement('p')
+        p.innerHTML = message.timestamp + " " + message.message
 
-            messageDiv.insertBefore(p, firstChild)
-        })
+        messageDiv.insertBefore(p, firstChild)
     })
+
+    if (data.length === 0) {
+        const firstChild = messageDiv.firstChild
+
+        const p = document.createElement('p')
+        p.innerHTML = "No message found"
+
+        messageDiv.insertBefore(p, firstChild)
+    }
 })
 
 sendBtn.onclick = async () => {
@@ -35,47 +45,56 @@ sendBtn.onclick = async () => {
 
     messageDiv.innerHTML = ""
 
-    fetch(`${endpoint}/msg`).then((res) => {
-        res.json().then((data) => {
+    const messages = await fetchJSON(`${endpoint}/msg`)
 
-            data.map(message => {
-                const firstChild = messageDiv.firstChild
+    messages.map(message => {
+        const firstChild = messageDiv.firstChild
 
-                const p = document.createElement('p')
-                p.innerHTML = message.timestamp + " " + message.message
+        const p = document.createElement('p')
+        p.innerHTML = message.timestamp + " " + message.message
 
-                messageDiv.insertBefore(p, firstChild)
-            })
-        })
+        messageDiv.insertBefore(p, firstChild)
     })
+
+    if (messages.length === 0) {
+        const firstChild = messageDiv.firstChild
+
+        const p = document.createElement('p')
+        p.innerHTML = "No message found"
+
+        messageDiv.insertBefore(p, firstChild)
+    }
 }
 
 searchBtn.onclick = async () => {
     const searchdata = { message: document.getElementById("searchBox").value }
 
-    await fetch(`${endpoint}/msg/search`, {
+    searchDiv.innerHTML = ""
+
+    fetch(`${endpoint}/msg/search`, {
         body: JSON.stringify(searchdata), // must match 'Content-Type' header
         headers: {
             'content-type': 'application/json'
         },
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    })
+    }).then((res) => {
+        return res.json()
+    }).then((data) => {
+        data.map(message => {
+            const firstChild = searchDiv.firstChild
 
-    searchDiv.innerHTML = ""
+            const p = document.createElement('p')
+            p.innerHTML = message.timestamp + " " + message.message
 
-    fetch(`${endpoint}/msg/search`).then((res) => {
-        res.json().then((data) => {
-
-            data.map(message => {
-                const firstChild = searchDiv.firstChild
-
-                const p = document.createElement('p')
-                p.innerHTML = message.timestamp + " " + message.message
-
-                searchDiv.insertBefore(p, firstChild)
-            })
+            searchDiv.insertBefore(p, firstChild)
         })
-    })
+        if (data.length === 0) {
+            const firstChild = searchDiv.firstChild
     
-
+            const p = document.createElement('p')
+            p.innerHTML = "No message found"
+    
+            searchDiv.insertBefore(p, firstChild)
+        }
+    })
 }
